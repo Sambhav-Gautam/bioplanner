a5728f45752bfa2f94cfcf241773ac6cfbaac358
Sambhav Gautam <126962720+Sambhav-Gautam@users.noreply.github.com>
2024-07-27 17:31:19 +0000

    Prompt for summarizing a protocol...

---
+++ prompts/summarizing_protocol.py
@@ -0,0 +1,10 @@
+summary_prompt = """
+Here is a molecular biology protocol entitled '{title}'.
+
+Here is the protocol abstract:
+{description}
+
+The protocol steps are as follows:
+{protocol_steps}
+
+Please provide a 2 sentence summary of the objective of this scientific protocol."""
